5860f9a7a4940af9bebc1d848a28ba7cb441f501
Samuel Higor <hiigor07@hotmail.com>
2021-11-10 10:43:43 -0300

    att

--- appMeuperfil/App.js
+++ appMeuperfil/App.js
@@ -10,32 +10,47 @@ class App extends Component{
     return(
       <View>
  
- <Imagem largura={250} altura={250} />
+       <Imagem largura={250} altura={250} />
+       
+       <View>
 
- <Text style={{ fontSize: 18, textAlignVertical: "center",textAlign: "center", fontWeight: 'bold', margin: 5 }}> Dados Pessoais: </Text>
+       <Text style={{ fontSize: 18, textAlignVertical: "center",textAlign: "center", fontWeight: 'bold', margin: 5 }}> Dados Pessoais: </Text>
 
- <Text style={{margin: 2, fontSize: 14 }}> Nome: Samuel Higor yarCorreia da Silva </Text>
- <Text style={{margin: 2, fontSize: 14 }}> Idade: 20 Anos </Text>
- <Text style={{margin: 2, fontSize: 14 }}> Data Nasc.: 24/05/2001 </Text>
+       <Text style={{margin: 2, fontSize: 14 }}> Nome: Samuel Higor yarCorreia da Silva </Text>
+       <Text style={{margin: 2, fontSize: 14 }}> Idade: 20 Anos </Text>
+       <Text style={{margin: 2, fontSize: 14 }}> Data Nasc.: 24/05/2001 </Text>
+
+       </View>
+       
+       <View>
+
+       <Text style={{ fontSize: 18, textAlignVertical: "center",textAlign: "center", fontWeight: 'bold', margin: 5 }}> Formação: </Text>
+
+       <Text style={{margin: 2, fontSize: 14 }}> Educação: Ensino Superior Cursando </Text>
+       <Text style={{margin: 2, fontSize: 14 }}> Cursos: Informática, Inglês, Gestão Empresarial </Text>
+
+       </View>
+
+       <View>
+       <Text style={{ fontSize: 18, textAlignVertical: "center",textAlign: "center", fontWeight: 'bold', margin: 5 }}> Experiência: </Text>
+
+       <Text style={{margin: 2, fontSize: 14 }}> - Exército Brasileiro - Administração </Text>
+
+       </View>
+
+       <View>
+
+       <Text style={{ fontSize: 18, textAlignVertical: "center",textAlign: "center", fontWeight: 'bold', margin: 5 }}> Projetos: </Text>
+
+       <Text style={{margin: 2, fontSize: 14 }}> - Sistema de Pesquisa de Produtos na WEB</Text>
+       
+
+       </View>
 
 
- <Text style={{ fontSize: 18, textAlignVertical: "center",textAlign: "center", fontWeight: 'bold', margin: 5 }}> Formação: </Text>
-
- <Text style={{margin: 2, fontSize: 14 }}> Educação: Ensino Superior Cursando </Text>
- <Text style={{margin: 2, fontSize: 14 }}> Cursos: Informática, Inglês, Gestão Empresarial </Text>
- 
- <Text style={{ fontSize: 18, textAlignVertical: "center",textAlign: "center", fontWeight: 'bold', margin: 5 }}> Experiência: </Text>
-
-<Text style={{margin: 2, fontSize: 14 }}> - Exército Brasileiro - Administração </Text>
-
-<Text style={{ fontSize: 18, textAlignVertical: "center",textAlign: "center", fontWeight: 'bold', margin: 5 }}> Projetos: </Text>
-
-<Text style={{margin: 2, fontSize: 14 }}> - Sistema de Pesquisa de Produtos na WEB </Text>
-
-
-</View>
-)
-}
+      </View>
+    )
+  }
 }
 
 class Imagem extends Component{
@@ -44,7 +59,7 @@ class Imagem extends Component{
 
       <Image 
       source={{uri: 'https://i.imgur.com/tkjGSYD.jpg'}}
-      style={{ width: this.props.largura, height: this.props.altura, margin: 50}}
+      style={{ width: this.props.largura, height: this.props.altura, margin: 50, left: 20}}
       />
     )
   }
